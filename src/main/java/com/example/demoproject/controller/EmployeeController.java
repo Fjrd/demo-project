7066b2cfa4e65b5d970a4f60ae218a815643bee3
Alexander <alexander_reztsov@mail.ru>
2021-10-26 19:43:05 +0300

    controller rewritten for simplicity

--- src/main/java/com/example/demoproject/controller/EmployeeController.java
+++ src/main/java/com/example/demoproject/controller/EmployeeController.java
@@ -1,7 +1,63 @@
 package com.example.demoproject.controller;
 
 import com.example.demoproject.domain.Employee;
+import com.example.demoproject.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-public interface EmployeeController extends GenericController<Employee> {
-    //employee specific endpoints
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAll() {
+        List<Employee> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> create(@RequestBody @Validated Employee json, UriComponentsBuilder uriComponentsBuilder) {
+        Employee saved = repository.save(json);
+        URI uri = uriComponentsBuilder
+                .path("/api/employees/{id}")
+                .buildAndExpand(saved.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getById(@PathVariable UUID id) {
+        Employee finded = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        return ResponseEntity.ok(finded);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> update(@PathVariable UUID id, @RequestBody @Validated Employee json, UriComponentsBuilder uriComponentsBuilder) {
+        repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        json.setId(id);
+        Employee updated = repository.save(json);
+        URI uri = uriComponentsBuilder
+                .path("/api/employees/{id}")
+                .buildAndExpand(updated.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable UUID id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        repository.delete(employee);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/example/demoproject/domain/Employee.java
+++ src/main/java/com/example/demoproject/domain/Employee.java
@@ -3,42 +3,54 @@ package com.example.demoproject.domain;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.UUID;
 
-@Getter
-@Setter(AccessLevel.PRIVATE)
+@Data
 @Builder(toBuilder = true)
-@ToString(doNotUseGetters = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Validated
 @Entity
 @Table(name = "employees")
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, doNotUseGetters = true, callSuper = true)
-public class Employee extends BaseEntity {
+public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    protected UUID id;
+
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
 
     @Min(0)
     @Max(120)
+    @NotNull
     private Integer age;
+
+    @NotNull
     private Double experience;
 
+    @NotNull
     @Column(name = "job_id")
     private Position position;
 
+    @NotNull
     @Column(name = "project_id")
     private Project project;
 
+    @NotNull
     @Column(name = "hire_date")
     private LocalDate hireDate;
 
+    @NotNull
     @Column(name = "gender_id")
     private Gender gender;
 }
