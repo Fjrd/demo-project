e187aa878a7cb74f9503e092b7c062e877bf5f3d
Alexander <alexander_reztsov@mail.ru>
2021-11-01 14:04:36 +0300

    added logging to controller and service

--- src/main/java/com/example/demoproject/controller/EmployeeController.java
+++ src/main/java/com/example/demoproject/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.demoproject.controller;
 import com.example.demoproject.dto.EmployeeDto;
 import com.example.demoproject.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
@@ -21,38 +23,49 @@ public class EmployeeController {
 
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAll() {
+        log.info("getAll() - start");
         List<EmployeeDto> list = service.findAll();
+        log.info("getAll(), list = {}", list);
         return ResponseEntity.ok(list);
     }
 
     @PostMapping
     public ResponseEntity<EmployeeDto> create(@RequestBody @Validated EmployeeDto dto, UriComponentsBuilder uriComponentsBuilder) {
+        log.info("create() - start, dto = {}, uriComponentsBuilder = {}", dto, uriComponentsBuilder);
         EmployeeDto saved = service.save(dto);
+        log.info("create(), saved = {}", saved);
         URI uri = uriComponentsBuilder
                 .path("/api/employees/{id}")
                 .buildAndExpand(saved.getId())
                 .toUri();
+        log.info("create(), uri = {}", uri);
         return ResponseEntity.created(uri).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeDto> getById(@PathVariable UUID id) {
+        log.info("getById() - start, id = {}", id);
         EmployeeDto dto = service.getById(id);
+        log.info("getById(), dto = {}", dto);
         return ResponseEntity.ok(dto);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeDto> update(@PathVariable UUID id, @RequestBody @Validated EmployeeDto dto, UriComponentsBuilder uriComponentsBuilder) {
+        log.info("update() - start, id = {}, dto = {}, uriComponentsBuilder = {}", id, dto, uriComponentsBuilder);
         EmployeeDto updated = service.update(id, dto);
+        log.info("update(), updated = {}", updated);
         URI uri = uriComponentsBuilder
                 .path("/api/employees/{id}")
                 .buildAndExpand(updated.getId())
                 .toUri();
+        log.info("update(), uri = {}", uri);
         return ResponseEntity.created(uri).build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteById(@PathVariable UUID id) {
+        log.info("deleteById() - start, id = {}", id);
         service.delete(id);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/example/demoproject/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/demoproject/service/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.demoproject.dto.EmployeeDto;
 import com.example.demoproject.mapper.EmployeeMapper;
 import com.example.demoproject.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -20,35 +22,47 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<EmployeeDto> findAll() {
-        return repository.findAll()
+        log.info("findAll() - start");
+        List<EmployeeDto> list = repository.findAll()
                 .stream()
                 .map(mapper::modelToDto)
                 .collect(Collectors.toList());
+        log.info("findAll(), list = {}", list);
+        return list;
     }
 
     @Override
     public EmployeeDto save(EmployeeDto dto) {
+        log.info("save() - start, dto = {}", dto);
         Employee saved = repository.save(mapper.dtoToModel(dto));
+        log.info("save(), saved = {}", saved);
         return mapper.modelToDto(saved);
     }
 
     @Override
     public EmployeeDto getById(UUID id) {
+        log.info("getById() - start, id = {}", id);
         Employee finded = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        log.info("getById(), finded = {}", finded);
         return mapper.modelToDto(finded);
     }
 
     @Override
     public void delete(UUID id) {
-        repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        log.info("delete() - start, id = {}", id);
+        Employee finded = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        log.info("delete(), finded = {}", finded);
         repository.deleteById(id);
     }
 
     @Override
     public EmployeeDto update(UUID id, EmployeeDto dto) {
-        repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        log.info("update(), id = {}, dto = {}", id, dto);
+        Employee finded = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id.toString()));
+        log.info("update(), finded = {}", finded);
         dto.setId(id);
         Employee saved = repository.save(mapper.dtoToModel(dto));
+        log.info("update(), saved = {}", saved);
         return mapper.modelToDto(saved);
     }
 }
